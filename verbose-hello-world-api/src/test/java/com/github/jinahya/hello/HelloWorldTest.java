2c5d914322c317f0eb08bf9b7b1dea5ed6aeb1fe
Jin Kwon <onacit@wemakeprice.com>
2019-04-23 11:43:00 +0900

    update

--- verbose-hello-world-api/src/test/java/com/github/jinahya/hello/HelloWorldTest.java
+++ verbose-hello-world-api/src/test/java/com/github/jinahya/hello/HelloWorldTest.java
@@ -278,17 +278,17 @@ public class HelloWorldTest {
         when(helloWorld.set(any(), anyInt())).thenAnswer(i -> i.getArgument(0));
     }
 
-    /**
-     * Asserts {@link HelloWorld#set(byte[], int)} method of {@link #helloWorld} returns specified {@code array}
-     * argument.
-     */
-    @Test
-    void assertSetArrayWithIndexReturnsSpecifiedArray() {
-        final byte[] array = current().nextBoolean() ? null : new byte[current().nextInt(HelloWorld.SIZE << 1)];
-        final int index = current().nextInt();
-        assertEquals(array, helloWorld.set(array, index));
-        verify(helloWorld).set(array, index);
-    }
+//    /**
+//     * Asserts {@link HelloWorld#set(byte[], int)} method of {@link #helloWorld} returns specified {@code array}
+//     * argument.
+//     */
+//    @Test
+//    void assertSetArrayWithIndexReturnsSpecifiedArray() {
+//        final byte[] array = current().nextBoolean() ? null : new byte[current().nextInt(HelloWorld.SIZE << 1)];
+//        final int index = current().nextInt();
+//        assertEquals(array, helloWorld.set(array, index));
+//        verify(helloWorld).set(array, index);
+//    }
 
     // -----------------------------------------------------------------------------------------------------------------
     @Spy
