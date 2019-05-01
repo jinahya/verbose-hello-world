3bd31bf1c514a6fea3ab6e1d44bb5d868235f5b3
Jin Kwon <onacit@gmail.com>
2019-05-01 21:12:05 +0900

    updated

--- verbose-hello-world-lib/src/test/java-hk2/com/github/jinahya/hello/HelloWorldImplInjectHk2Binder.java
+++ verbose-hello-world-lib/src/test/java-hk2/com/github/jinahya/hello/HelloWorldImplInjectHk2Binder.java
@@ -1,8 +1,14 @@
 package com.github.jinahya.hello;
 
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
 
 class HelloWorldImplInjectHk2Binder extends AbstractBinder {
+
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @Override
     protected void configure() {
