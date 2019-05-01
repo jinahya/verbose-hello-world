8865a0acdeb14473a367021b29ee7cb438e161b5
Jin Kwon <onacit@gmail.com>
2019-05-01 16:47:18 +0900

    updated

--- verbose-hello-world-lib/src/test/java/com/github/jinahya/hello/HelloWorldImplTest.java
+++ verbose-hello-world-lib/src/test/java/com/github/jinahya/hello/HelloWorldImplTest.java
@@ -1,7 +1,5 @@
 package com.github.jinahya.hello;
 
-import org.junit.jupiter.api.Test;
-
 /**
  * A class for testing {@link HelloWorldImpl}.
  */
@@ -9,50 +7,7 @@ public class HelloWorldImplTest extends AbstractHelloWorldImplTest {
 
     @Override
     HelloWorld helloWorld() {
-        return new HelloWorldImpl();
-    }
-
-    /**
-     * Asserts {@link HelloWorldImpl#set(byte[], int)} method throws a {@code NullPointerException} when {@code array}
-     * argument is {@code null}.
-     */
-    @Test
-    public void assertSetArrayThrowsNullPointerExceptionIfArrayIsNull() {
         // TODO: implement!
-    }
-
-    /**
-     * Asserts {@link HelloWorldImpl#set(byte[], int)} method throws an {@code IndexOutOfBoundsException} when {@code
-     * index} argument is negative.
-     */
-    @Test
-    public void assertSetArrayThrowsIndexOutOfBoundsExceptionWhenIndexIsNegative() {
-        // TODO: implement!
-    }
-
-    /**
-     * Asserts {@link HelloWorldImpl#set(byte[], int)} method throws an {@code IndexOutOfBoundsException} when {@code
-     * index} argument plus {@link HelloWorld#SIZE} is greater than {@code array.length}.
-     */
-    @Test
-    public void assertSetArrayThrowsIndexOutOfBoundsExceptionWhenIndexPlusHelloWorldSizeIsGreaterThanArrayLength() {
-        // TODO: implement!
-    }
-
-    /**
-     * Asserts {@link HelloWorldImpl#set(byte[], int)} method sets hello world bytes on specified array starting at
-     * specified index.
-     */
-    @Test
-    public void assertSetArraySetsHelloWorldBytesOnArrayStartingAtIndex() {
-        // TODO: implement!
-    }
-
-    /**
-     * Asserts {@link HelloWorldImpl#set(byte[], int)} method returns specified array.
-     */
-    @Test
-    public void assertSetArrayReturnsSpecifiedArray() {
-        // TODO: implement!
+        return null;
     }
 }
