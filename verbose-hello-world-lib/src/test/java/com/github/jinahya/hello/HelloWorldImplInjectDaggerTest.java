7ef607cc358e4a74b9b0e3f2fa1bddf51233eaf4
Jin Kwon <onacit@gmail.com>
2019-05-01 17:46:23 +0900

    updated

--- verbose-hello-world-lib/src/test/java/com/github/jinahya/hello/HelloWorldImplInjectDaggerTest.java
+++ verbose-hello-world-lib/src/test/java/com/github/jinahya/hello/HelloWorldImplInjectDaggerTest.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
 
-class HelloWorldImplInjectDagger1Test extends HelloWorldImplInjectTest {
+class HelloWorldImplInjectDaggerTest extends HelloWorldImplInjectTest {
 
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 }
