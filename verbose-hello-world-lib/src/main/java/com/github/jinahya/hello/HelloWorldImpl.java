f3843e7d2c4d79b39827072f5e01f74ebf77a8f3
Jin Kwon <onacit@gmail.com>
2019-05-01 14:37:22 +0900

    updated

---
+++ verbose-hello-world-lib/src/main/java/com/github/jinahya/hello/HelloWorldImpl.java
@@ -0,0 +1,13 @@
+package com.github.jinahya.hello;
+
+/**
+ * An implementation of {@link HelloWorld}.
+ */
+public class HelloWorldImpl implements HelloWorld {
+
+    @Override
+    public byte[] set(final byte[] array, int index) {
+        // TODO: implement!
+        return null;
+    }
+}
---
+++ verbose-hello-world-lib/src/test/java/com/github/jinahya/hello/HelloWorldImplTest.java
@@ -0,0 +1,33 @@
+package com.github.jinahya.hello;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldImplTest {
+
+    /**
+     * Asserts {@link HelloWorldImpl#set(byte[], int)} method throws a {@code NullPointerException} when {@code array}
+     * argument is {@code null}.
+     */
+    @Test
+    public void assertSetArrayThrowsNullPointerExceptionIfArrayIsNull() {
+        // TODO: implement!
+    }
+
+    /**
+     * Asserts {@link HelloWorldImpl#set(byte[], int)} method throws an {@code IndexOutOfBoundsException} when {@code
+     * index} argument is negative.
+     */
+    @Test
+    public void assertSetArrayThrowsIndexOutOfBoundsExceptionWhenIndexIsNegative() {
+        // TODO: implement!
+    }
+
+    /**
+     * Asserts {@link HelloWorldImpl#set(byte[], int)} method throws an {@code IndexOutOfBoundsException} when {@code
+     * index} argument plus {@link HelloWorld#SIZE} is greater than {@code array.length}.
+     */
+    @Test
+    public void assertSetArrayThrowsIndexOutOfBoundsExceptionWhenIndexPlusHelloWorldSizeIsGreaterThanArrayLength() {
+        // TODO: implement!
+    }
+}
