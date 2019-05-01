bc3ab233989d7f9e05fcc2a20584e894e1bcb03f
Jin Kwon <onacit@gmail.com>
2019-05-01 17:04:25 +0900

    updated

---
+++ verbose-hello-world-lib/src/test/java/com/github/jinahya/hello/HelloWorldImplServiceTest.java
@@ -0,0 +1,10 @@
+package com.github.jinahya.hello;
+
+class HelloWorldImplServiceTest extends AbstractHelloWorldImplTest {
+
+    @Override
+    HelloWorld helloWorld() {
+        // TODO: implement!
+        return null;
+    }
+}
