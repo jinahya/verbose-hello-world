39bfa972b581c6d85816b24991d364b9ef5a34b2
Jin Kwon <onacit@wemakeprice.com>
2019-05-02 11:28:36 +0900

    updated

---
+++ verbose-hello-world-app1/src/main/java/com/github/jinahya/hello/HelloWorldMain.java
@@ -0,0 +1,19 @@
+package com.github.jinahya.hello;
+
+import java.io.IOException;
+
+/**
+ * A class whose {@link #main(String[])} method prints {@code hello, world} to {@link System#out}.
+ */
+public class HelloWorldMain {
+
+    /**
+     * The main method of this program which prints {@code hello, world} followed by a line delimiter.
+     *
+     * @param args an array of main arguments
+     * @throws IOException if an I/O error occurs.
+     */
+    public static void main(final String[] args) throws IOException {
+        // TODO: implement!
+    }
+}
