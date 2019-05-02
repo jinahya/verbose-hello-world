ad5a05eb437b1e3b1936df3ed802f415961724b1
Jin Kwon <onacit@wemakeprice.com>
2019-05-02 15:03:31 +0900

    updated

---
+++ verbose-hello-world-app/verbose-hello-world-app3/src/main/java/com/github/jinahya/hello/HelloWorldMain.java
@@ -0,0 +1,23 @@
+package com.github.jinahya.hello;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.util.logging.Logger;
+
+/**
+ * A class whose {@link #main(String[])} method prints {@code hello, world} to {@link System#out}.
+ */
+public class HelloWorldMain {
+
+    private static final Logger logger = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
+
+    /**
+     * The main method of this program which prints {@code hello, world} followed by a line delimiter.
+     *
+     * @param args an array of main arguments
+     * @throws IOException if an I/O error occurs.
+     */
+    public static void main(final String[] args) throws IOException {
+        // TODO: implement!
+    }
+}
