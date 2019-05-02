22a0f74eb184b5cb26c60704f056a694c8a631db
Jin Kwon <onacit@wemakeprice.com>
2019-05-02 15:30:40 +0900

    updated

--- verbose-hello-world-app/verbose-hello-world-app3/src/main/java/com/github/jinahya/hello/HelloWorldModule.java
+++ verbose-hello-world-app/verbose-hello-world-app3/src/main/java/com/github/jinahya/hello/HelloWorldModule.java
@@ -1,14 +1,23 @@
 package com.github.jinahya.hello;
 
 import com.google.inject.AbstractModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
 
 class HelloWorldModule extends AbstractModule {
+
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private static final String NAME = "com.github.jinahya.hello.HelloWorldImpl";
 
     static final Class<?> HELLO_WORLD_IMPL;
 
     static {
         try {
-            HELLO_WORLD_IMPL = Class.forName("com.github.jinahya.hello.HelloWorldImpl");
+            HELLO_WORLD_IMPL = Class.forName(NAME);
+            logger.debug("found: {}", HELLO_WORLD_IMPL);
         } catch (final ClassNotFoundException cnfe) {
             throw new InstantiationError(cnfe.getMessage());
         }
