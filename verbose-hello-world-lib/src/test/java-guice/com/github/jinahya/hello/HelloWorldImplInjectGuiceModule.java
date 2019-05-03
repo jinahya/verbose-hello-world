97675839d39e42ed1ac9e4c595a2bd9d012eed7b
Jin Kwon <onacit@wemakeprice.com>
2019-05-03 15:51:04 +0900

    updated

--- verbose-hello-world-lib/src/test/java-guice/com/github/jinahya/hello/HelloWorldImplInjectGuiceModule.java
+++ verbose-hello-world-lib/src/test/java-guice/com/github/jinahya/hello/HelloWorldImplInjectGuiceModule.java
@@ -2,10 +2,22 @@ package com.github.jinahya.hello;
 
 import com.google.inject.AbstractModule;
 
+import static com.github.jinahya.hello.HelloWorldImplInjectTest.QUALIFIER_DEMO;
+import static com.github.jinahya.hello.HelloWorldImplInjectTest.QUALIFIER_IMPL;
+import static com.google.inject.name.Names.named;
+import static java.util.concurrent.ThreadLocalRandom.current;
+
+/**
+ * A Guice module for injecting {@link HelloWorld} instances.
+ */
 class HelloWorldImplInjectGuiceModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(HelloWorld.class).to(HelloWorldImpl.class);
+        bind(HelloWorld.class).to(current().nextBoolean() ? HelloWorldImpl.class : HelloWorldDemo.class);
+        bind(HelloWorld.class).annotatedWith(named(QUALIFIER_IMPL)).to(HelloWorldImpl.class);
+        bind(HelloWorld.class).annotatedWith(named(QUALIFIER_DEMO)).to(HelloWorldDemo.class);
+        bind((HelloWorld.class)).annotatedWith(ImplQualifier.class).to(HelloWorldImpl.class);
+        bind((HelloWorld.class)).annotatedWith(DemoQualifier.class).to(HelloWorldDemo.class);
     }
 }
--- verbose-hello-world-lib/src/test/java-guice/com/github/jinahya/hello/HelloWorldImplInjectGuiceTest.java
+++ verbose-hello-world-lib/src/test/java-guice/com/github/jinahya/hello/HelloWorldImplInjectGuiceTest.java
@@ -2,13 +2,17 @@ package com.github.jinahya.hello;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import dagger.ObjectGraph;
 import org.junit.jupiter.api.BeforeEach;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
 
+/**
+ * An injection test for Google Guice.
+ *
+ * @see <a href="https://github.com/google/guice">Guice</a>
+ */
 class HelloWorldImplInjectGuiceTest extends HelloWorldImplInjectTest {
 
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
