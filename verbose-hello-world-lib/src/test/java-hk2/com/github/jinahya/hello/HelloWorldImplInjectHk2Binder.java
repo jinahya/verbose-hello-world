8d2ba22e8356f64a04b00bfb7ae279c56090cd49
Jin Kwon <onacit@wemakeprice.com>
2019-05-03 15:58:46 +0900

    updated

--- verbose-hello-world-lib/src/test/java-hk2/com/github/jinahya/hello/HelloWorldImplInjectHk2Binder.java
+++ verbose-hello-world-lib/src/test/java-hk2/com/github/jinahya/hello/HelloWorldImplInjectHk2Binder.java
@@ -1,17 +1,41 @@
 package com.github.jinahya.hello;
 
+import org.glassfish.hk2.api.AnnotationLiteral;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
 
+import static com.github.jinahya.hello.HelloWorldImplInjectTest.QUALIFIER_DEMO;
+import static com.github.jinahya.hello.HelloWorldImplInjectTest.QUALIFIER_IMPL;
+import static java.util.concurrent.ThreadLocalRandom.current;
+
+/**
+ * A binder for injecting {@link HelloWorld}s.
+ */
 class HelloWorldImplInjectHk2Binder extends AbstractBinder {
 
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+    private static class ImplLiteral extends AnnotationLiteral<ImplQualifier> implements ImplQualifier {
+
+    }
+
+    private static class DemoLiteral extends AnnotationLiteral<DemoQualifier> implements DemoQualifier {
+
+    }
+
     @Override
     protected void configure() {
-        bind(HelloWorldImpl.class).to(HelloWorld.class);
+        if (current().nextBoolean()) {
+            bind(HelloWorldImpl.class).to(HelloWorld.class);
+        } else {
+            bind(HelloWorldDemo.class).to(HelloWorld.class);
+        }
+        bind(HelloWorldImpl.class).named(QUALIFIER_IMPL).to(HelloWorld.class);
+        bind(HelloWorldDemo.class).named(QUALIFIER_DEMO).to(HelloWorld.class);
+        bind(HelloWorldImpl.class).qualifiedBy(new ImplLiteral()).to(HelloWorld.class);
+        bind(HelloWorldImpl.class).qualifiedBy(new DemoLiteral()).to(HelloWorld.class);
     }
 }
