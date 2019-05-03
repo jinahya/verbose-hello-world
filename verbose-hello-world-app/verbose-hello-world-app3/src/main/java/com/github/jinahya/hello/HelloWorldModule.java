6b86682ee04a61b6c3da9411335fe65df5ba6b3a
Jin Kwon <onacit@wemakeprice.com>
2019-05-03 14:15:52 +0900

    updated

--- verbose-hello-world-app/verbose-hello-world-app3/src/main/java/com/github/jinahya/hello/HelloWorldModule.java
+++ verbose-hello-world-app/verbose-hello-world-app3/src/main/java/com/github/jinahya/hello/HelloWorldModule.java
@@ -6,12 +6,21 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
 
+/**
+ * A module for injecting instances of {@link HelloWorld}.
+ */
 class HelloWorldModule extends AbstractModule {
 
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+    /**
+     * The fully qualified name of the {@code HelloWorldMain} class.
+     */
     private static final String HELLO_WORLD_IMPL_NAME = "com.github.jinahya.hello.HelloWorldImpl";
 
+    /**
+     * The class found with {@link #HELLO_WORLD_IMPL_NAME}.
+     */
     static final Class<?> HELLO_WORLD_IMPL_CLASS;
 
     static {
