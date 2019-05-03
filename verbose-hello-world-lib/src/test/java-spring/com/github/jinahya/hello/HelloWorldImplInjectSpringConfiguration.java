4e8300f54e0b5a4d4fbf10cf799064995748dcd2
Jin Kwon <onacit@wemakeprice.com>
2019-05-03 16:03:22 +0900

    updated

--- verbose-hello-world-lib/src/test/java-spring/com/github/jinahya/hello/HelloWorldImplInjectSpringConfiguration.java
+++ verbose-hello-world-lib/src/test/java-spring/com/github/jinahya/hello/HelloWorldImplInjectSpringConfiguration.java
@@ -5,15 +5,45 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.inject.Named;
 import java.lang.invoke.MethodHandles;
 
+import static java.util.concurrent.ThreadLocalRandom.current;
+
+/**
+ * A configuration for providing {@link HelloWorld} beans.
+ */
 @Configuration
 class HelloWorldImplInjectSpringConfiguration {
 
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @Bean
-    HelloWorld helloWorld() {
+    HelloWorld any() {
+        return current().nextBoolean() ? new HelloWorldImpl() : new HelloWorldDemo();
+    }
+
+    @Named(HelloWorldImplInjectTest.QUALIFIER_IMPL)
+    @Bean
+    HelloWorld namedImpl() {
         return new HelloWorldImpl();
+    }
+
+    @Named(HelloWorldImplInjectTest.QUALIFIER_DEMO)
+    @Bean
+    HelloWorld namedDemo() {
+        return new HelloWorldDemo();
+    }
+
+    @ImplQualifier
+    @Bean
+    HelloWorld qualifiedImpl() {
+        return new HelloWorldImpl();
+    }
+
+    @DemoQualifier
+    @Bean
+    HelloWorld qualifiedDemo() {
+        return new HelloWorldDemo();
     }
 }
