f7f9f5553fab6b8ce397c3dcdb8cbfaf85cc9143
Jin Kwon <onacit@gmail.com>
2019-05-04 17:17:10 +0900

    updated

--- verbose-hello-world-lib/src/test/java/com/github/jinahya/hello/HelloWorldImplInjectTest.java
+++ verbose-hello-world-lib/src/test/java/com/github/jinahya/hello/HelloWorldImplInjectTest.java
@@ -6,13 +6,6 @@ import org.slf4j.LoggerFactory;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.lang.invoke.MethodHandles;
-import java.lang.reflect.Field;
-import java.util.List;
-
-import static java.util.Arrays.stream;
-import static java.util.Collections.shuffle;
-import static java.util.stream.Collectors.toList;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 abstract class HelloWorldImplInjectTest extends AbstractHelloWorldImplTest {
 
@@ -30,23 +23,8 @@ abstract class HelloWorldImplInjectTest extends AbstractHelloWorldImplTest {
 
     @Override
     HelloWorld helloWorld() {
-        final List<Field> fields = stream(getClass().getDeclaredFields())
-                .filter(f -> HelloWorld.class.equals(f.getType()))
-                .filter(f -> f.isAnnotationPresent(Inject.class))
-                .collect(toList());
-        assertFalse(fields.isEmpty());
-        final List<HelloWorld> values = fields.stream()
-                .map(f -> {
-                    try {
-                        return (HelloWorld) f.get(this);
-                    } catch (final IllegalAccessException iae) {
-                        throw new RuntimeException(iae);
-                    }
-                })
-                .collect(toList());
-        assertFalse(values.isEmpty());
-        shuffle(values);
-        return values.get(0);
+        // TODO: implement!
+        return null;
     }
 
     @Inject
