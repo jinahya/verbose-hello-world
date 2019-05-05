710f346586f70d1d90953786e6da191b8e5fd135
Jin Kwon <onacit@gmail.com>
2019-05-05 22:40:24 +0900

    updated

---
+++ verbose-hello-world-app/verbose-hello-world-app4/src/main/java/com/github/jinahya/hello/HelloWorldMain.java
@@ -0,0 +1,30 @@
+package com.github.jinahya.hello;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.util.logging.Logger;
+
+/**
+ * A class whose {@link #main(String[])} method accepts socket connections and writes {@code hello, world} to clients.
+ */
+public class HelloWorldMain {
+
+    private static final Logger logger = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
+
+    /**
+     * The main method of this program which accepts socket connections and writes {@code hello, world} to clients.
+     *
+     * @param args an array of command line arguments
+     * @throws IOException if an I/O error occurs.
+     */
+    public static void main(final String[] args) throws IOException {
+        // TODO: implement!
+    }
+
+    /**
+     * Creates a new instance.
+     */
+    private HelloWorldMain() {
+        super();
+    }
+}
