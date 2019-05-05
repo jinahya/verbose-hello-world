79c77dcae57fc8869afee0bd59ddb0b35717783b
Jin Kwon <onacit@gmail.com>
2019-05-05 15:54:30 +0900

    updated

--- verbose-hello-world-lib/src/test/java-weld-junit5/com/github/jinahya/hello/HelloWorldWeldJunit5Test.java
+++ verbose-hello-world-lib/src/test/java-weld-junit5/com/github/jinahya/hello/HelloWorldWeldJunit5Test.java
@@ -4,8 +4,8 @@ import org.jboss.weld.junit5.WeldJunit5Extension;
 import org.jboss.weld.junit5.auto.AddBeanClasses;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@AddBeanClasses({HelloWorldImplInjectWeldFactory.class})
+@AddBeanClasses({HelloWorldCdiFactory.class})
 @ExtendWith({WeldJunit5Extension.class})
-class HelloWorldImplInjectWeldTest extends HelloWorldImplInjectTest {
+class HelloWordWeldJunit5Test extends HelloWorldDiTest {
 
 }
