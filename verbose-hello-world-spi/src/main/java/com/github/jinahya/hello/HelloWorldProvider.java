6bcb2e98ff7bea6095e47d87cc888ba597e19219
Jin Kwon <onacit@gmail.com>
2019-05-06 14:20:00 +0900

    updated

--- verbose-hello-world-spi/src/main/java/com/github/jinahya/hello/HelloWorldProvider.java
+++ verbose-hello-world-spi/src/main/java/com/github/jinahya/hello/HelloWorldProvider.java
@@ -1,9 +1,9 @@
 package com.github.jinahya.hello;
 
+import java.util.ServiceLoader;
+import java.util.Spliterator;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
-
-import static java.util.ServiceLoader.load;
 
 /**
  * An interface for hello world provider.
@@ -11,17 +11,25 @@ import static java.util.ServiceLoader.load;
 public interface HelloWorldProvider {
 
     /**
-     * Returns a stream of hello world providers filtered by {@link #isAvailable()}.
+     * Returns a stream of hello world providers.
      *
      * @return a stream of hello world providers
      */
-    static Stream<HelloWorldProvider> availableProviders() {
-        return StreamSupport.stream(load(HelloWorldProvider.class).spliterator(), false)
-                .filter(HelloWorldProvider::isAvailable);
+    static Stream<HelloWorldProvider> providerStream() {
+        final Iterable<HelloWorldProvider> loader = ServiceLoader.load(HelloWorldProvider.class);
+        final Spliterator<HelloWorldProvider> spliterator = loader.spliterator();
+        return StreamSupport.stream(spliterator, false);
     }
 
+    /**
+     * Find any available hello world provider and returns the value of {@link #getAvailable()}.
+     *
+     * @return an available instance of {@link HelloWorld}.
+     */
     static HelloWorld findAnyAvailableAndGetOrElseThrow() {
-        return availableProviders().findAny().orElseThrow(() -> new RuntimeException("no available services"))
+        return providerStream()
+                .filter(HelloWorldProvider::isAvailable)
+                .findAny().orElseThrow(() -> new RuntimeException("no available services"))
                 .getAvailable();
     }
 
