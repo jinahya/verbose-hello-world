9cdd59d294f5f52de7231b6aefabd83afb106b35
Jin Kwon <onacit@gmail.com>
2019-05-06 20:38:20 +0900

    updated

---
+++ verbose-hello-world-app/verbose-hello-world-srv4/src/main/java/com/github/jinahya/hello/HelloWorldMain.java
@@ -0,0 +1,35 @@
+package com.github.jinahya.hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+
+import static com.github.jinahya.hello.HelloWorldProvider.findAnyAvailableAndGetOrElseThrow;
+
+/**
+ * A class whose {@link #main(String[])} method accepts socket connections and sends {@code hello, world} to clients.
+ */
+public class HelloWorldMain {
+
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    /**
+     * The main method of this program which accepts socket connections and sends {@code hello, world} to clients.
+     *
+     * @param args an array of command line arguments
+     * @throws IOException if an I/O error occurs.
+     */
+    public static void main(final String[] args) throws IOException {
+        final HelloWorld service = findAnyAvailableAndGetOrElseThrow();
+        // TODO: implement!
+    }
+
+    /**
+     * Creates a new instance.
+     */
+    private HelloWorldMain() {
+        super();
+    }
+}
---
+++ verbose-hello-world-app/verbose-hello-world-srv4/src/main/java/com/github/jinahya/hello/HelloWorldProviderImpl.java
@@ -0,0 +1,14 @@
+package com.github.jinahya.hello;
+
+public class HelloWorldProviderImpl implements HelloWorldProvider {
+
+    @Override
+    public HelloWorld getHelloWorld() {
+        try {
+            return Class.forName("com.github.jinahya.hello.HelloWorldImpl").asSubclass(HelloWorld.class)
+                    .getConstructor().newInstance();
+        } catch (final ReflectiveOperationException roe) {
+            throw new RuntimeException("failed to instantiate HelloWorldImpl", roe);
+        }
+    }
+}
