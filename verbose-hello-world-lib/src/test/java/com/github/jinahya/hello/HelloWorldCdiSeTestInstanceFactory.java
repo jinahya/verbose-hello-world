6ec73ee114506704a5885a639c4f73690bc351b3
Jin Kwon <onacit@gmail.com>
2019-11-24 18:50:54 +0900

    Update javadoc

--- verbose-hello-world-lib/src/test/java/com/github/jinahya/hello/HelloWorldCdiSeTestInstanceFactory.java
+++ verbose-hello-world-lib/src/test/java/com/github/jinahya/hello/HelloWorldCdiSeTestInstanceFactory.java
@@ -1,30 +1,31 @@
 package com.github.jinahya.hello;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestInstanceFactory;
 import org.junit.jupiter.api.extension.TestInstanceFactoryContext;
 import org.junit.jupiter.api.extension.TestInstantiationException;
-import org.slf4j.Logger;
 
 import javax.enterprise.inject.se.SeContainer;
 import javax.enterprise.inject.se.SeContainerInitializer;
 
-import static java.lang.invoke.MethodHandles.lookup;
-import static org.slf4j.LoggerFactory.getLogger;
-
+/**
+ * A test instance factory for {@link HelloWorldCdiSeTest} class.
+ *
+ * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ */
+@Slf4j
 class HelloWorldCdiSeTestInstanceFactory implements TestInstanceFactory {
 
-    private static final Logger logger = getLogger(lookup().lookupClass());
-
+    // -----------------------------------------------------------------------------------------------------------------
     @Override
     public Object createTestInstance(final TestInstanceFactoryContext testInstanceFactoryContext,
                                      final ExtensionContext extensionContext)
             throws TestInstantiationException {
         final Class<?> testClass = testInstanceFactoryContext.getTestClass();
-        final SeContainerInitializer initializer = SeContainerInitializer.newInstance()
-//                .disableDiscovery()
+        final SeContainerInitializer seContainerInitializer = SeContainerInitializer.newInstance()
                 .addBeanClasses(HelloWorldCdiFactory.class, testClass);
-        try (SeContainer container = initializer.initialize()) {
+        try (SeContainer container = seContainerInitializer.initialize()) {
             return container.select(testClass).get();
         }
     }
