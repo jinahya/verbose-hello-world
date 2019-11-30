9fbee34ae40e92e61b1a567020637116975123f7
Jin Kwon <onacit@gmail.com>
2019-11-30 14:11:38 +0900

    update

--- verbose-hello-world-api/src/main/java/com/github/jinahya/hello/HelloWorld.java
+++ verbose-hello-world-api/src/main/java/com/github/jinahya/hello/HelloWorld.java
@@ -104,13 +104,12 @@ public interface HelloWorld {
     }
 
     /**
-     * Appends <a href="#hello-world-bytes">hello-world-bytes</a> to specified file and returns the file.
+     * Writes <a href="#hello-world-bytes">hello-world-bytes</a> to specified file and returns the file.
      * <p>
-     * This method creates an instance of {@link FileOutputStream}, with {@link FileOutputStream#FileOutputStream(File,
-     * boolean) appending mode}, from specified file and invokes {@link #write(OutputStream)} method with it and {@link
-     * OutputStream#flush() flushes} the stream.
+     * This method creates an instance of {@link FileOutputStream}, as an {@link FileOutputStream#FileOutputStream(File,
+     * boolean) appending mode}, from specified file and invokes {@link #write(OutputStream)} method with it.
      *
-     * @param file the file to which bytes are appended.
+     * @param file the file to which bytes are written.
      * @param <T>  file type parameter
      * @return specified file.
      * @throws NullPointerException if {@code file} is {@code null}.
