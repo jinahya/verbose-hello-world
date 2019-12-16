2de4f209e6123c85d2209546b855db6dd3900fb9
Jin Kwon <onacit@wemakeprice.com>
2019-12-16 13:16:30 +0900

    Configure maven-helper-plugin and update the docker script

--- docker.maven.sh
+++ docker.maven.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 if [ $# -lt 3 ]; then
-  echo "Usage: $1 <tag> <goal1> <goal2> ..."
+  echo "Usage: $0  <tag> <phases...>, e.g. $0 3-jdk-11-openj9 clean install"
   exit 1
 fi
+groupId=$(mvn help:evaluate -Dexpression=project.groupId -q -DforceStdout)
+artifactId=$(mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout)
+version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 tag="$1"
 shift
-docker run -it --rm --name verbose-hello-world -v "$(pwd)":/usr/src/mymaven -v "$HOME/.m2":/root/.m2 -w /usr/src/mymaven maven:"$tag" mvn "$@"
+docker run -it --rm \
+  --name verbose-hello-world \
+  -v "$HOME/.m2":/root/.m2 \
+  -v "$(pwd)":/usr/src/$groupId/$artifactId/$version \
+  -w /usr/src/$groupId/$artifactId/$version \
+  maven:"$tag" mvn "$@"
