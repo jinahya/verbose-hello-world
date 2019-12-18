91662d5e19cc136358dfbd81c470c588edd7cd7a
Jin Kwon <onacit@wemakeprice.com>
2019-12-18 11:35:32 +0900

    updated

--- verbose-hello-world-api/src/main/java/com/github/jinahya/hello/AsynchronousHelloWorld.java
+++ verbose-hello-world-api/src/main/java/com/github/jinahya/hello/AsynchronousHelloWorld.java
@@ -32,6 +32,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 import static java.nio.ByteBuffer.allocate;
+import static java.util.Objects.requireNonNull;
 
 /**
  * An extended hello world interface for asynchronous operations.
@@ -45,28 +46,28 @@ interface AsynchronousHelloWorld extends HelloWorld {
     /**
      * Returns a byte buffer contains the {@code hello-world-bytes}. The result buffer's {@code position} is {@code 0}
      * and the {@code limit} is {@value com.github.jinahya.hello.HelloWorld#BYTES}. This method invokes {@link
-     * #put(ByteBuffer)} with an empty byte buffer of {@value com.github.jinahya.hello.HelloWorld#BYTES} bytes and
-     * returns the buffer after {@link ByteBuffer#flip() flips} it.
+     * #put(ByteBuffer)} with a newly allocated byte buffer of {@value com.github.jinahya.hello.HelloWorld#BYTES} bytes
+     * and returns the buffer after {@link ByteBuffer#flip() flips} it.
      *
      * @return a byte buffer contains the {@code hello-world-bytes}.
+     * @see HelloWorld#put(ByteBuffer)
      */
     default ByteBuffer put() {
-        final ByteBuffer buffer = allocate(BYTES); // position = 0, limit,capacity = 12
-        put(buffer); // position -> 12
-        buffer.flip(); // limit -> position(12), position -> 0
-        return buffer;
+        return (ByteBuffer) put(allocate(BYTES)).flip();
     }
 
     // ----------------------------------------------------------------------------------------- AsynchronousFileChannel
 
     /**
-     * Writes the {@code hello-world-bytes} to specified channel starting at the specified file position.
+     * Writes the {@code hello-world-bytes} to specified channel starting at the given file position. This method
+     * invokes {@link #put()} and writes all remaining bytes of the result buffer starting at the given file position.
      *
      * @param channel the channel to be appended.
      * @param <T>     channel type parameter
-     * @return specified channel
+     * @return specified channel.
      * @throws InterruptedException if interrupted while {@link Future#get() getting} the result from a {@code Future}.
      * @throws ExecutionException   if failed to {@link Future#get() get} the result from a {@code Future}.
+     * @see #put()
      * @see AsynchronousFileChannel#write(ByteBuffer, long)
      */
     default <T extends AsynchronousFileChannel> @NotNull T write(@NotNull final T channel, long position)
@@ -85,8 +86,8 @@ interface AsynchronousHelloWorld extends HelloWorld {
 
     /**
      * Appends the {@code hello-world-bytes} to the end of specified channel. This method invokes {@link
-     * #write(AsynchronousFileChannel, long)} method with given {@code channel} and the value of {@link
-     * AsynchronousFileChannel#size() size(channel)} and returns the result.
+     * #write(AsynchronousFileChannel, long)} method with given {@code channel} and the current {@link
+     * AsynchronousFileChannel#size() size} of the channel and returns the result.
      *
      * @param channel the channel to be appended.
      * @param <T>     channel type parameter
@@ -94,16 +95,22 @@ interface AsynchronousHelloWorld extends HelloWorld {
      * @throws IOException          if an I/O error occurs.
      * @throws InterruptedException if interrupted while working.
      * @throws ExecutionException   if failed to execute.
+     * @see AsynchronousFileChannel#size()
      * @see #write(AsynchronousFileChannel, long)
      */
     default <T extends AsynchronousFileChannel> @NotNull T append(@NotNull final T channel)
             throws IOException, InterruptedException, ExecutionException {
-        if (channel == null) {
-            throw new NullPointerException("channel is null");
-        }
-        return write(channel, channel.size());
+        return write(requireNonNull(channel, "channel is null"), channel.size());
     }
 
+    /**
+     * Writes, asynchronously, the {@code hello-world-bytes} to specified channel starting at given file position.
+     *
+     * @param channel  the channel to which bytes are written.
+     * @param position the file position to start with writing the bytes.
+     * @param <T>      channel type parameter
+     * @return a completable future of specified channel.
+     */
     default <T extends AsynchronousFileChannel> @NotNull CompletableFuture<T> writeAsync(@NotNull final T channel,
                                                                                          final long position) {
         if (channel == null) {
@@ -134,15 +141,30 @@ interface AsynchronousHelloWorld extends HelloWorld {
         return future;
     }
 
+    /**
+     * Appends, asynchronously, the {@code hello-world-bytes} to specified channel starting at the end of the file.
+     *
+     * @param channel the channel to which bytes are written.
+     * @param <T>     channel type parameter
+     * @return a completable future of specified channel.
+     */
     default <T extends AsynchronousFileChannel> @NotNull CompletableFuture<T> appendAsync(@NotNull final T channel)
             throws IOException {
-        if (channel == null) {
-            throw new NullPointerException("channel is null");
-        }
-        return writeAsync(channel, channel.size());
+        return writeAsync(requireNonNull(channel, "channel is null"), channel.size());
     }
 
     // ----------------------------------------------------------------------------------------- AsynchronousByteChannel
+
+    /**
+     * Writes the {@code hello-world-bytes} to specified channel.
+     *
+     * @param channel the channel to which bytes are written.
+     * @param <T>     channel type parameter
+     * @return given channel.
+     * @throws InterruptedException if interrupted while working.
+     * @throws ExecutionException   if failed to execute.
+     * @see #writeAsync(AsynchronousByteChannel)
+     */
     default <T extends AsynchronousByteChannel> @NotNull T write(@NotNull final T channel)
             throws InterruptedException, ExecutionException {
         if (channel == null) {
@@ -155,6 +177,14 @@ interface AsynchronousHelloWorld extends HelloWorld {
         return channel;
     }
 
+    /**
+     * Writes, asynchronously, the {@code hello-world-bytes} to specified channel.
+     *
+     * @param channel the channel to which bytes are written.
+     * @param <T>     channel type parameter
+     * @return a completable future of given channel.
+     * @see #write(AsynchronousByteChannel)
+     */
     default <T extends AsynchronousByteChannel> CompletableFuture<T> writeAsync(@NotNull final T channel) {
         if (channel == null) {
             throw new NullPointerException("channel is null");
@@ -181,18 +211,33 @@ interface AsynchronousHelloWorld extends HelloWorld {
     }
 
     // --------------------------------------------------------------------------------------- AsynchronousSocketChannel
+
+    /**
+     * Sends the {@code hello-world-bytes} to specified channel. This method simply invokes {@link
+     * #write(AsynchronousByteChannel)} method with specified channel and returns the result.
+     *
+     * @param channel the channel to which bytes are written.
+     * @param <T>     channel type parameter
+     * @return given channel.
+     * @throws InterruptedException if interrupted while working.
+     * @throws ExecutionException   if failed to execute.
+     * @see #write(AsynchronousByteChannel)
+     */
     default <T extends AsynchronousSocketChannel> @NotNull T send(@NotNull final T channel)
             throws InterruptedException, ExecutionException {
-        if (channel == null) {
-            throw new NullPointerException("channel is null");
-        }
-        return write(channel);
+        return write(requireNonNull(channel, "channel is null"));
     }
 
+    /**
+     * Sends, asynchronously, the {@code hello-world-bytes} to specified channel. This method simply invokes {@link
+     * #writeAsync(AsynchronousByteChannel)} method with specified channel and returns the result.
+     *
+     * @param channel the channel to which bytes are written.
+     * @param <T>     channel type parameter
+     * @return given channel.
+     * @see #write(AsynchronousByteChannel)
+     */
     default <T extends AsynchronousSocketChannel> CompletableFuture<T> sendAsync(@NotNull final T channel) {
-        if (channel == null) {
-            throw new NullPointerException("channel is null");
-        }
-        return writeAsync(channel);
+        return writeAsync(requireNonNull(channel, "channel is null"));
     }
 }
