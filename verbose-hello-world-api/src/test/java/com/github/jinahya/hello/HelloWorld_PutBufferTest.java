f66d047c00c9d46b42b50dffafbae31f85a7038e
Jin Kwon <onacit@wemakeprice.com>
2021-03-19 12:26:23 +0900

    updated

--- verbose-hello-world-api/src/test/java/com/github/jinahya/hello/HelloWorld_PutBufferTest.java
+++ verbose-hello-world-api/src/test/java/com/github/jinahya/hello/HelloWorld_PutBufferTest.java
@@ -25,12 +25,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 
 import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.quality.Strictness.LENIENT;
@@ -44,6 +49,66 @@ import static org.mockito.quality.Strictness.LENIENT;
 @ExtendWith({MockitoExtension.class})
 @Slf4j
 class HelloWorld_PutBufferTest extends AbstractHelloWorldTest {
+
+    /**
+     * Returns a stream of byte buffers which each remaining is less than {@link HelloWorld#BYTES}.
+     *
+     * @return a stream of byte buffers which each remaining is less than {@link HelloWorld#BYTES}.
+     */
+    private static Stream<ByteBuffer> buffersOfNotEnoughRemaining() {
+        return Stream
+                .of(ByteBuffer.wrap(new byte[ThreadLocalRandom.current().nextInt(HelloWorld.BYTES)]),
+                    ByteBuffer.allocate(ThreadLocalRandom.current().nextInt(HelloWorld.BYTES)),
+                    ByteBuffer.allocateDirect(ThreadLocalRandom.current().nextInt(HelloWorld.BYTES)))
+                .peek(b -> {
+                    Assertions.assertTrue(b.remaining() < HelloWorld.BYTES);
+                });
+    }
+
+    /**
+     * Returns a stream of byte buffers which each has a backing array.
+     *
+     * @return a stream of byte buffers which each has a backing array.
+     */
+    private static Stream<ByteBuffer> buffersHasBackingArray() {
+        return IntStream.range(0, 8)
+                .mapToObj(i -> {
+                    final int capacity = ThreadLocalRandom.current().nextInt(HelloWorld.BYTES, HelloWorld.BYTES << 1);
+                    final ByteBuffer buffer = ByteBuffer.wrap(new byte[capacity]);
+                    Assertions.assertEquals(capacity, buffer.capacity());
+                    Assertions.assertEquals(capacity, buffer.limit());
+                    Assertions.assertEquals(0, buffer.position());
+                    Assertions.assertTrue(buffer.remaining() >= HelloWorld.BYTES);
+                    if (buffer.limit() > HelloWorld.BYTES) {
+                        buffer.position(ThreadLocalRandom.current().nextInt(buffer.limit() - HelloWorld.BYTES));
+                        Assertions.assertTrue(buffer.remaining() >= HelloWorld.BYTES);
+                    }
+                    return buffer;
+                });
+    }
+
+    /**
+     * Returns a stream of byte buffers which each has no backing array.
+     *
+     * @return a stream of byte buffers which each has no backing array.
+     */
+    private static Stream<ByteBuffer> buffersHasNoBackingArray() {
+        return IntStream.range(0, 8)
+                .mapToObj(i -> {
+                    final int capacity = ThreadLocalRandom.current().nextInt(HelloWorld.BYTES, HelloWorld.BYTES << 1);
+                    final ByteBuffer buffer = ThreadLocalRandom.current().nextBoolean()
+                                              ? ByteBuffer.allocate(capacity) : ByteBuffer.allocateDirect(capacity);
+                    Assertions.assertEquals(capacity, buffer.capacity());
+                    Assertions.assertEquals(capacity, buffer.limit());
+                    Assertions.assertEquals(0, buffer.position());
+                    Assertions.assertTrue(buffer.remaining() >= HelloWorld.BYTES);
+                    if (buffer.limit() > HelloWorld.BYTES) {
+                        buffer.position(ThreadLocalRandom.current().nextInt(buffer.limit() - HelloWorld.BYTES));
+                        Assertions.assertTrue(buffer.remaining() >= HelloWorld.BYTES);
+                    }
+                    return buffer;
+                });
+    }
 
     /**
      * Asserts {@link HelloWorld#put(ByteBuffer)} method throws a {@link NullPointerException} when {@code buffer}
@@ -61,8 +126,9 @@ class HelloWorld_PutBufferTest extends AbstractHelloWorldTest {
      * com.github.jinahya.hello.HelloWorld#BYTES}).
      */
     @DisplayName("put(buffer) throws BufferOverflowException when buffer.remaining is less than BYTES")
-    @Test
-    void putBuffer_BufferOverflowException_BufferRemainingIsNotEnough() {
+    @MethodSource({"buffersOfNotEnoughRemaining"})
+    @ParameterizedTest
+    void putBuffer_BufferOverflowException_BufferRemainingIsNotEnough(final ByteBuffer buffer) {
     }
 
     /**
@@ -72,8 +138,9 @@ class HelloWorld_PutBufferTest extends AbstractHelloWorldTest {
      * {@value HelloWorld#BYTES}.
      */
     @DisplayName("put(buffer-with-backing-array) invokes set(buffer.array, buffer.arrayOffset + buffer.position)")
-    @Test
-    void putBuffer_InvokeSetArrayWithIndexAndIncrementPosition_BufferHasBackingArray() {
+    @MethodSource({"buffersHasBackingArray"})
+    @ParameterizedTest
+    void putBuffer_InvokeSetArrayWithIndexAndIncrementPosition_BufferHasBackingArray(final ByteBuffer buffer) {
     }
 
     /**
@@ -91,9 +158,9 @@ class HelloWorld_PutBufferTest extends AbstractHelloWorldTest {
      * Asserts {@link HelloWorld#put(ByteBuffer)} method returns given buffer when the buffer has a backing array.
      */
     @DisplayName("put(buffer-with-backing-array) returns buffer")
-    @Test
-    void putBuffer_ReturnBuffer_BufferHasBackingArray() {
-        final ByteBuffer expected = ByteBuffer.wrap(new byte[HelloWorld.BYTES]);
+    @MethodSource({"buffersHasBackingArray"})
+    @ParameterizedTest
+    void putBuffer_ReturnBuffer_BufferHasBackingArray(final ByteBuffer expected) {
         final ByteBuffer actual = helloWorld.put(expected);
         Assertions.assertSame(expected, actual);
     }
@@ -102,10 +169,9 @@ class HelloWorld_PutBufferTest extends AbstractHelloWorldTest {
      * Asserts {@link HelloWorld#put(ByteBuffer)} method returns given buffer when the buffer has no backing array.
      */
     @DisplayName("put(buffer with no backing array) returns specified buffer")
-    @Test
-    void putBuffer_ReturnBuffer_BufferHasNoBackingArray() {
-        final ByteBuffer expected = Mockito.mock(ByteBuffer.class);
-        Mockito.when(expected.hasArray()).thenReturn(Boolean.FALSE);
+    @MethodSource({"buffersHasBackingArray"})
+    @ParameterizedTest
+    void putBuffer_ReturnBuffer_BufferHasNoBackingArray(final ByteBuffer expected) {
         final ByteBuffer actual = helloWorld.put(expected);
         Assertions.assertSame(expected, actual);
     }
