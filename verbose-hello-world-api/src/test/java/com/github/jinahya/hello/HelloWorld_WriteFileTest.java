d63d7a35fe2d90128fc8447e3b142c38d27b7331
Jin Kwon <onacit@wemakeprice.com>
2021-03-19 10:52:22 +0900

    Add HelloWorld_WriteFileTest

---
+++ verbose-hello-world-api/src/test/java/com/github/jinahya/hello/HelloWorld_WriteFileTest.java
@@ -0,0 +1,77 @@
+package com.github.jinahya.hello;
+
+/*-
+ * #%L
+ * verbose-hello-world-api
+ * %%
+ * Copyright (C) 2018 - 2019 Jinahya, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import static org.mockito.quality.Strictness.LENIENT;
+
+/**
+ * A class for unit-testing {@link HelloWorld} interface.
+ *
+ * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
+ */
+@MockitoSettings(strictness = LENIENT)
+@ExtendWith({MockitoExtension.class})
+@Slf4j
+class HelloWorld_WriteFileTest extends AbstractHelloWorldTest {
+
+    /**
+     * Asserts {@link HelloWorld#write(RandomAccessFile)} method throws a {@link NullPointerException} when {@code file}
+     * argument is {@code null}.
+     */
+    @DisplayName("write(file) throws NullPointerException when file is null")
+    @Test
+    void writeFile_NullPointerException_FileIsNull() {
+        Assertions.assertThrows(NullPointerException.class, () -> helloWorld.write((RandomAccessFile) null));
+    }
+
+    /**
+     * Asserts {@link HelloWorld#write(RandomAccessFile)} invokes {@link HelloWorld#set(byte[])} method with an array of
+     * {@value com.github.jinahya.hello.HelloWorld#BYTES} bytes and invokes {@link RandomAccessFile#write(byte[])}
+     * method on {@code file} with the array.
+     *
+     * @throws IOException if an I/O error occurs.
+     */
+    @DisplayName("write(file) invokes set(array) method and writes the array to file")
+    @Test
+    void writeFile_InvokeSetArrayWriteArrayToFile_() throws IOException {
+    }
+
+    /**
+     * Asserts {@link HelloWorld#write(RandomAccessFile)} method returns specified {@code file}.
+     *
+     * @throws IOException if an I/O error occurs.
+     */
+    @DisplayName("write(file) returns file")
+    @Test
+    void writeFile_ReturnFile_() throws IOException {
+    }
+}
