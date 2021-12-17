5c4c5b4fb14dcc844a6fd95211f4a698a95305a9
Jin Kwon <onacit@users.noreply.github.com>
2021-12-17 15:45:54 +0900

    wip

--- verbose-hello-world-app/verbose-hello-world-app4/src/main/java/com/github/jinahya/hello/HelloWorldMain.java
+++ verbose-hello-world-app/verbose-hello-world-app4/src/main/java/com/github/jinahya/hello/HelloWorldMain.java
@@ -36,9 +36,10 @@ import java.io.IOException;
 public class HelloWorldMain {
 
     /**
-     * The main method of this program which prints {@code hello, world} followed by a new line character.
+     * The main method of this program which prints {@code hello, world} to {@link System#out} followed by a new line
+     * character.
      *
-     * @param args an array of command line arguments
+     * @param args an array of main arguments
      * @throws IOException if an I/O error occurs.
      */
     public static void main(final String... args) throws IOException {
