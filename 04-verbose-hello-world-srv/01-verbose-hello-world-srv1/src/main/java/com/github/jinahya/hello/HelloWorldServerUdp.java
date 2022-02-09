68229ffb102db53aac318f9f197b0448f25accc1
Jin Kwon <onacit@gmail.com>
2022-02-09 15:36:55 +0900

    wip

--- 04-verbose-hello-world-srv/01-verbose-hello-world-srv1/src/main/java/com/github/jinahya/hello/HelloWorldServerUdp.java
+++ 04-verbose-hello-world-srv/01-verbose-hello-world-srv1/src/main/java/com/github/jinahya/hello/HelloWorldServerUdp.java
@@ -27,6 +27,9 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.net.SocketTimeoutException;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
  * A class serves {@code hello, world} to clients.
@@ -64,11 +67,14 @@ class HelloWorldServerUdp
         }
         log.info("server bound to {}", socket.getLocalSocketAddress());
         PORT.set(socket.getLocalPort());
+        socket.setSoTimeout((int) SECONDS.toMicros(1L));
         new Thread(() -> {
             while (!socket.isClosed()) {
                 final var clientPacket = new DatagramPacket(new byte[0], 0);
                 try {
                     socket.receive(clientPacket);
+                } catch (final SocketTimeoutException ste) {
+                    continue;
                 } catch (final IOException ioe) {
                     if (socket.isClosed()) {
                         break;
