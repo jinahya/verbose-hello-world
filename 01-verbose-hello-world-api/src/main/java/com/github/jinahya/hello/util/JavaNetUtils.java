3b82b263e22d9dda2a8647ad78be3b2d4242f568
Jin Kwon <onacit@gmail.com>
2023-11-11 21:03:15 +0900

    wip

--- 01-verbose-hello-world-api/src/main/java/com/github/jinahya/hello/util/JavaNetUtils.java
+++ 01-verbose-hello-world-api/src/main/java/com/github/jinahya/hello/util/JavaNetUtils.java
@@ -1,46 +1,104 @@
 package com.github.jinahya.hello.util;
 
-import java.io.UncheckedIOException;
 import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.function.BiFunction;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public final class JavaNetUtils {
 
-    public static <R> R applyLoopbackAddressesIPv4(
-            final Function<? super InetAddress, ? extends R> function) {
-        var address = InetAddress.getLoopbackAddress();
-        if (address instanceof Inet6Address) {
-            try {
-                address = InetAddress.getByName("127.0.0.1");
-            } catch (final UnknownHostException uhe) {
-                throw new UncheckedIOException(uhe);
-            }
-        }
-        return function.apply(address);
+    public static void acceptNetworkInterfaces(final Predicate<? super NetworkInterface> predicate,
+                                               final Consumer<? super NetworkInterface> consumer)
+            throws SocketException {
+        Objects.requireNonNull(predicate, "predicate is null");
+        Objects.requireNonNull(consumer, "consumer is null");
+        Collections.list(NetworkInterface.getNetworkInterfaces())
+                .stream()
+                .filter(predicate)
+                .forEach(consumer::accept);
     }
 
-    public static <R> R applyLoopbackAddressesIPv6(
-            final Function<? super InetAddress, ? extends R> function) {
-        var address = InetAddress.getLoopbackAddress();
-        if (address instanceof Inet4Address) {
-            try {
-                address = InetAddress.getByName("::1");
-            } catch (final UnknownHostException uhe) {
-                throw new UncheckedIOException(uhe);
-            }
-        }
-        return function.apply(address);
-    }
-
-    public static <R> R applyLoopbackAddresses(
-            final BiFunction<? super InetAddress, ? super InetAddress, ? extends R> function) {
-        return applyLoopbackAddressesIPv4(
-                ipv4 -> applyLoopbackAddressesIPv6(ipv6 -> function.apply(ipv4, ipv6))
+    public static void acceptInetAddresses(
+            final Predicate<? super NetworkInterface> interfacePredicate,
+            final BiPredicate<? super NetworkInterface, ? super InetAddress> addressPredicate,
+            final BiConsumer<? super NetworkInterface, ? super InetAddress> consumer)
+            throws SocketException {
+        Objects.requireNonNull(interfacePredicate, "interfacePredicate is null");
+        Objects.requireNonNull(addressPredicate, "addressPredicate is null");
+        Objects.requireNonNull(consumer, "consumer is null");
+        acceptNetworkInterfaces(
+                interfacePredicate,
+                ni -> ni.inetAddresses()
+                        .filter(ia -> addressPredicate.test(ni, ia))
+                        .forEach(ia -> consumer.accept(ni, ia))
         );
+    }
+
+    public static Stream<NetworkInterface> networkInterfaceStream(
+            final Predicate<? super NetworkInterface> predicate)
+            throws SocketException {
+        Objects.requireNonNull(predicate, "predicate is null");
+        return Collections.list(NetworkInterface.getNetworkInterfaces())
+                .stream().filter(predicate);
+    }
+
+    public static Stream<InetAddress> inetAddressStream(
+            final Predicate<? super NetworkInterface> interfacePredicate,
+            final BiPredicate<? super NetworkInterface, ? super InetAddress> addressPredicate)
+            throws SocketException {
+        Objects.requireNonNull(interfacePredicate, "interfacePredicate is null");
+        Objects.requireNonNull(addressPredicate, "addressPredicate is null");
+        return networkInterfaceStream(interfacePredicate)
+                .flatMap(ni -> ni.inetAddresses().filter(ia -> addressPredicate.test(ni, ia)));
+    }
+
+    public static Stream<InetAddress> loopbackAddressStream(
+            final Predicate<? super NetworkInterface> interfacePredicate)
+            throws SocketException {
+        return inetAddressStream(
+                interfacePredicate,
+                (ni, ia) -> ia.isLoopbackAddress()
+        );
+    }
+
+    public static Stream<InetAddress> loopbackAddressStreamIPv4(
+            final Predicate<? super NetworkInterface> interfacePredicate)
+            throws SocketException {
+        return inetAddressStream(
+                interfacePredicate,
+                (ni, ia) -> ia.isLoopbackAddress() && ia instanceof Inet4Address
+        );
+    }
+
+    public static Stream<InetAddress> loopbackAddressStreamIPv6(
+            final Predicate<? super NetworkInterface> interfacePredicate)
+            throws SocketException {
+        return inetAddressStream(
+                interfacePredicate,
+                (ni, ia) -> ia.isLoopbackAddress() && ia instanceof Inet6Address
+        );
+    }
+
+    public static <R> Optional<R> applyFirstLoopbackAddressIPv4(
+            final Function<? super InetAddress, ? extends R> function)
+            throws SocketException {
+        return loopbackAddressStreamIPv4(ni -> true).findFirst().map(function);
+    }
+
+    public static <R> Optional<R> applyFirstLoopbackAddressIPv6(
+            final Function<? super InetAddress, ? extends R> function)
+            throws SocketException {
+        return loopbackAddressStreamIPv6(ni -> true).findFirst().map(function);
     }
 
     private JavaNetUtils() {
--- 01-verbose-hello-world-api/src/test/java/com/github/jinahya/hello/util/JavaNetUtilsTest.java
+++ 01-verbose-hello-world-api/src/test/java/com/github/jinahya/hello/util/JavaNetUtilsTest.java
@@ -1,33 +1,102 @@
 package com.github.jinahya.hello.util;
 
-import org.assertj.core.api.InstanceOfAssertFactories;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.net.Inet4Address;
 import java.net.Inet6Address;
-import java.net.InetAddress;
+import java.net.SocketException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 class JavaNetUtilsTest {
 
+    @DisplayName("acceptInetAddress")
     @Nested
-    class ApplyLoopbackAddressesTest {
+    class AcceptInetAddressesTest {
 
+        @DisplayName("loopbackAddresses")
         @Test
-        void __() {
-            JavaNetUtils.applyLoopbackAddresses((ipv4, ipv6) -> {
-                assertThat(ipv4)
-                        .isInstanceOf(Inet4Address.class)
-                        .extracting(InetAddress::isLoopbackAddress,
-                                    InstanceOfAssertFactories.BOOLEAN).isTrue();
-                assertThat(ipv6)
-                        .isInstanceOf(Inet6Address.class)
-                        .extracting(InetAddress::isLoopbackAddress,
-                                    InstanceOfAssertFactories.BOOLEAN).isTrue();
-                return null;
+        void __() throws SocketException {
+            JavaNetUtils.acceptInetAddresses(
+                    ni -> {
+                        return true;
+                    },
+                    (ni, ia) -> {
+                        return ia.isLoopbackAddress();
+                    },
+                    (ni, ia) -> {
+                        log.debug("networkInterface: {}, inetAddress: {}", ni, ia);
+                    }
+            );
+        }
+    }
+
+    @DisplayName("loopbackAddressStream")
+    @Nested
+    class LoopbackAddressStreamTest {
+
+        @DisplayName("loopbackAddresses")
+        @Test
+        void __() throws SocketException {
+            JavaNetUtils.loopbackAddressStream(ni -> true).forEach(la -> {
+                log.debug("loopbackAddress: {}, {}", la, la.getClass());
+                assertThat(la.isLoopbackAddress()).isTrue();
             });
+        }
+
+        @DisplayName("loopbackAddressesIPv4")
+        @Test
+        void __ipv4() throws SocketException {
+            JavaNetUtils.loopbackAddressStreamIPv4(ni -> true).forEach(la -> {
+                log.debug("loopbackAddressIPv4: {}, {}", la, la.getClass());
+                assertThat(la.isLoopbackAddress()).isTrue();
+                assertThat(la).isInstanceOf(Inet4Address.class);
+            });
+        }
+
+        @DisplayName("loopbackAddressesIPv6")
+        @Test
+        void __ipv6() throws SocketException {
+            JavaNetUtils.loopbackAddressStreamIPv6(ni -> true).forEach(la -> {
+                log.debug("loopbackAddressIPv6: {}, {}", la, la.getClass());
+                assertThat(la.isLoopbackAddress()).isTrue();
+                assertThat(la).isInstanceOf(Inet6Address.class);
+            });
+        }
+    }
+
+    @DisplayName("applyFirstLoopbackAddress")
+    @Nested
+    class ApplyFirstLoopbackAddressTest {
+
+        @DisplayName("applyFirstLoopbackAddressIPv4")
+        @Nested
+        class ApplyFirstLoopbackAddressIPv4Test {
+
+            @Test
+            void __() throws SocketException {
+                JavaNetUtils.applyFirstLoopbackAddressIPv4(ia -> {
+                    assertThat(ia).isInstanceOf(Inet4Address.class);
+                    return null;
+                });
+            }
+        }
+
+        @DisplayName("applyFirstLoopbackAddressIPv6")
+        @Nested
+        class ApplyFirstLoopbackAddressIPv6Test {
+
+            @Test
+            void __() throws SocketException {
+                JavaNetUtils.applyFirstLoopbackAddressIPv6(ia -> {
+                    assertThat(ia).isInstanceOf(Inet6Address.class);
+                    return null;
+                });
+            }
         }
     }
 }
