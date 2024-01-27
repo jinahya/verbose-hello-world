1d19cad1e9d5440257f1258cd660626b9806f99e
Jin Kwon <onacit@gmail.com>
2024-01-28 00:51:38 +0900

    wip

---
+++ 04-verbose-hello-world-srv/00-verbose-hello-world-srv-basic/.diff_rfc862tcp_rfc863tcp.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+diff \
+./src/main/java/com/github/jinahya/hello/misc/c01rfc863/Rfc863Tcp.java \
+./src/main/java/com/github/jinahya/hello/misc/c02rfc862/Rfc862Tcp.java
---
+++ 04-verbose-hello-world-srv/00-verbose-hello-world-srv-basic/.diff_rfc862udp_rfc863udp.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+diff \
+./src/main/java/com/github/jinahya/hello/misc/c01rfc863/Rfc863Udp.java \
+./src/main/java/com/github/jinahya/hello/misc/c02rfc862/Rfc862Udp.java
