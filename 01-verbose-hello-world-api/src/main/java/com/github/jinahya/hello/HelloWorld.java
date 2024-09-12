9024abff535884b5f2d650beb4ba8a24a08122d6
Jin Kwon <onacit@gmail.com>
2024-09-12 13:50:34 +0900

    wip

--- 01-verbose-hello-world-api/src/main/java/com/github/jinahya/hello/HelloWorld.java
+++ 01-verbose-hello-world-api/src/main/java/com/github/jinahya/hello/HelloWorld.java
@@ -309,7 +309,7 @@ public interface HelloWorld {
         if (output == null) {
             throw new NullPointerException("output is null");
         }
-        // get the hello, world bytes
+        // get the hello-world-bytes
         final var array = set(new byte[BYTES]);
         // invoke <output.write(array)>
 
@@ -348,9 +348,9 @@ public interface HelloWorld {
         if (file == null) {
             throw new NullPointerException("file is null");
         }
-        // get the hello, world bytes
+        // get the hello-world-bytes
         final var array = set(new byte[BYTES]);
-        // write array to <file>
+        // invoke <file.write(array)>
 
         // return the <file>
         return file;
@@ -381,7 +381,7 @@ public interface HelloWorld {
         if (writer == null) {
             throw new NullPointerException("writer is null");
         }
-        // invoke <append(writer)> with <writer>
+        // invoke <append(writer)>
 
         // return the <writer>
         return writer;
@@ -416,7 +416,7 @@ public interface HelloWorld {
             throw new NullPointerException("socket is null");
         }
         final var stream = socket.getOutputStream();
-        // invoke <write(stream)> with <stream>
+        // invoke <write(stream)>
 
         // return the <socket>
         return socket;
