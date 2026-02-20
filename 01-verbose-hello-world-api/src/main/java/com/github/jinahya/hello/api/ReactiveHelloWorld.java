a30645db56230ba83431c49950f3a7ed60e885a4
Jin Kwon <onacit@gmail.com>
2026-02-20 14:49:42 +0900

    wip

--- 01-verbose-hello-world-api/src/main/java/com/github/jinahya/hello/api/ReactiveHelloWorld.java
+++ 01-verbose-hello-world-api/src/main/java/com/github/jinahya/hello/api/ReactiveHelloWorld.java
@@ -1,5 +1,10 @@
 package com.github.jinahya.hello.api;
 
-interface ReactiveHelloWorld {
+import org.reactivestreams.Subscription;
 
+import java.util.concurrent.Flow;
+
+interface ReactiveHelloWorld<T> {
+
+    Subscription subscribe(Flow.Subscriber<? super T> subscriber);
 }
