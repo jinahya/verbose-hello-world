abdbf6a77b0ff67fcbc2ebf07eaebe2bf3d14792
Jin Kwon <onacit@gmail.com>
2026-05-04 04:21:08 +0900

    wip

--- 01-verbose-hello-world-api/src/test/java/com/github/jinahya/hello/api/_java_util_zip/HelloWorld_Update_Checksum_Test.java
+++ 01-verbose-hello-world-api/src/test/java/com/github/jinahya/hello/api/_java_util_zip/HelloWorld_Update_Checksum_Test.java
@@ -3,74 +3,54 @@ package com.github.jinahya.hello.api._java_util_zip;
 import com.github.jinahya.hello.api.HelloWorld;
 import com.github.jinahya.hello.api.HelloWorldTest;
 import com.github.jinahya.hello.api.HelloWorldTestUtils;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 
-import java.util.stream.Stream;
-import java.util.zip.Adler32;
-import java.util.zip.CRC32;
-import java.util.zip.CRC32C;
 import java.util.zip.Checksum;
 
 /**
- * Tests for {@link HelloWorld#update(Checksum)} method.
+ * A class for testing {@link HelloWorld#update(Checksum) update(checksum)} method.
  *
  * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
- * @see <a
- * href="https://docs.oracle.com/en/java/javase/25/docs/api/java.base/java/util/zip/Checksum.html">Checksum</a>
- * @see <a
- * href="https://docs.oracle.com/en/java/javase/25/docs/api/java.base/java/util/zip/CRC32.html">CRC32</a>
- * @see <a
- * href="https://docs.oracle.com/en/java/javase/25/docs/api/java.base/java/util/zip/CRC32C.html">CRC32C</a>
- * @see <a
- * href="https://docs.oracle.com/en/java/javase/25/docs/api/java.base/java/util/zip/Adler32.html">Adler32</a>
  */
+@DisplayName("update(Checksum)")
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
 @Slf4j
 class HelloWorld_Update_Checksum_Test
         extends HelloWorldTest {
 
-    // ---------------------------------------------------------------------------------------------
-    @BeforeEach
-    void __() {
-        HelloWorldTestUtils.set_array_sets_actual_hello_world_bytes(service());
-    }
-
+    @DisplayName("""
+            should throw a <NullPointerException>
+            when the <checksum> argument is <null>""")
     @Test
     void _ThrowNullPointerException_ChecksumIsNull() {
         // ----------------------------------------------------------------------------------- given
         final var service = service();
+        final Checksum checksum = null;
         // ----------------------------------------------------------------------------- when / then
-        Assertions.assertThrows(NullPointerException.class, () -> service.update((Checksum) null));
-    }
-
-    private static Stream<Checksum> getChecksumStream() {
-        return Stream.of(
-                Mockito.spy(new CRC32()),
-                Mockito.spy(new CRC32C()),
-                Mockito.spy(new Adler32())
+        Assertions.assertThrows(
+                NullPointerException.class,
+                () -> service.update(checksum)
         );
     }
 
-    @DisplayName("update(Checksum)")
-    @MethodSource("getChecksumStream")
-    @ParameterizedTest
-    void __(final Checksum checksum) {
+    @DisplayName("checksum.update(set(byte[12]))")
+    @Test
+    void __() {
         // ----------------------------------------------------------------------------------- given
-        final var service = service();
+        final var service = HelloWorldTestUtils.set_array_sets_random_bytes(service());
+        final var checksum = Mockito.mock(Checksum.class);
         // ------------------------------------------------------------------------------------ when
         final var result = service.update(checksum);
         // ------------------------------------------------------------------------------------ then
-        final var array = HelloWorldTestUtils.set_array12_invoked_once(service); // <1>
-        Mockito.verify(checksum, Mockito.times(1)).update(array);                // <2>
-        Assertions.assertSame(checksum, result);                                 // <3>
-        final var value = checksum.getValue();
-        log.debug("{}: 0x{}", String.format("%1$7s", checksum.getClass().getSimpleName()),
-                  Long.toHexString(value).toUpperCase());
+        final var array = HelloWorldTestUtils.set_array12_invoked_once(service);
+        Mockito.verify(checksum, Mockito.times(1)).update(array);
+        Mockito.verifyNoMoreInteractions(checksum);
+        Assertions.assertSame(checksum, result);
     }
 }
---
+++ 01-verbose-hello-world-api/src/test/java/com/github/jinahya/hello/api/_java_util_zip/HelloWorld_Update_Checksum__Test.java
@@ -0,0 +1,60 @@
+package com.github.jinahya.hello.api._java_util_zip;
+
+import com.github.jinahya.hello.api.HelloWorldTest;
+import com.github.jinahya.hello.api.HelloWorldTestUtils;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+
+import java.util.stream.Stream;
+import java.util.zip.Adler32;
+import java.util.zip.CRC32;
+import java.util.zip.CRC32C;
+import java.util.zip.Checksum;
+
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@Slf4j
+class HelloWorld_Update_Checksum__Test
+        extends HelloWorldTest {
+
+    private static Stream<Checksum> getChecksumStream() {
+        return Stream.of(
+                Mockito.spy(new CRC32()),
+                Mockito.spy(new CRC32C()),
+                Mockito.spy(new Adler32())
+        );
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    @BeforeEach
+    void __() {
+        Mockito.doAnswer(i -> {
+            final var checksum = i.getArgument(0, Checksum.class);
+            checksum.update(HelloWorldTestUtils.hello_world_byte_array());
+            return checksum;
+        }).when(service()).update(ArgumentMatchers.<Checksum>notNull());
+    }
+
+    // ---------------------------------------------------------------------------------------------
+    @DisplayName("update(Checksum)")
+    @MethodSource("getChecksumStream")
+    @ParameterizedTest
+    void __(final Checksum checksum) {
+        // ----------------------------------------------------------------------------------- given
+        final var service = service();
+        // ------------------------------------------------------------------------------------ when
+        final var result = service.update(checksum);
+        // ------------------------------------------------------------------------------------ then
+        Assertions.assertSame(checksum, result);
+        final var value = checksum.getValue();
+        System.out.printf("%7s: 0x%s%n", checksum.getClass().getSimpleName(),
+                          Long.toHexString(value));
+    }
+}
