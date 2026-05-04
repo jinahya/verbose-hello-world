b38342450b66eec008edb2bb12b5e738cd3271c8
Jin Kwon <onacit@gmail.com>
2026-05-05 00:00:49 +0900

    wip

--- 01-verbose-hello-world-api/src/test/java/com/github/jinahya/hello/api/_java_util_jar/HelloWorld_Java_Util_Jar__Test.java
+++ 01-verbose-hello-world-api/src/test/java/com/github/jinahya/hello/api/_java_util_jar/HelloWorld_Java_Util_Jar__Test.java
@@ -5,13 +5,20 @@ import com.github.jinahya.hello.api.HelloWorldTestUtils;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarInputStream;
+import java.util.jar.JarOutputStream;
 
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 @Slf4j
@@ -31,17 +38,53 @@ class HelloWorld_Java_Util_Jar__Test
     class JarOutputStream_Test {
 
         @Test
-        void __JarInputStream() {
-            // TODO: create a temp file
-            // TODO: write using write(ZipOutputStream)
-            // TODO: read using jar inputstream
+        void __JarInputStream() throws IOException {
+            // ----------------------------------------------------------------------------- given
+            final var tempFile = File.createTempFile("tmp", null, tempDir);
+            // ------------------------------------------------------------------------------ when
+            try (var fos = new FileOutputStream(tempFile);
+                 var jos = new JarOutputStream(fos)) {
+                jos.putNextEntry(new JarEntry("hello-world.txt"));
+                service().write(jos);
+                jos.closeEntry();
+                jos.flush();
+            }
+            // ------------------------------------------------------------------------------ then
+            try (var fis = new FileInputStream(tempFile);
+                 var jis = new JarInputStream(fis)) {
+                final var entry = jis.getNextJarEntry();
+                assert entry != null;
+                Assertions.assertArrayEquals(
+                        HelloWorldTestUtils.hello_world_byte_array(),
+                        jis.readAllBytes()
+                );
+            }
         }
 
         @Test
-        void __JarFile() {
-            // TODO: create a temp file
-            // TODO: write using write(ZipOutputStream)
-            // TODO: read using jar inputstream
+        void __JarFile() throws IOException {
+            // ----------------------------------------------------------------------------- given
+            final var tempFile = File.createTempFile("tmp", null, tempDir);
+            final var entryName = "hello-world.bin";
+            // ------------------------------------------------------------------------------ when
+            try (var fos = new FileOutputStream(tempFile);
+                 var jos = new JarOutputStream(fos)) {
+                jos.putNextEntry(new JarEntry(entryName));
+                service().write(jos);
+                jos.closeEntry();
+                jos.flush();
+            }
+            // ------------------------------------------------------------------------------ then
+            try (var jarFile = new JarFile(tempFile)) {
+                final var entry = jarFile.getJarEntry(entryName);
+                assert entry != null;
+                try (var in = jarFile.getInputStream(entry)) {
+                    Assertions.assertArrayEquals(
+                            HelloWorldTestUtils.hello_world_byte_array(),
+                            in.readAllBytes()
+                    );
+                }
+            }
         }
     }
 }
--- 01-verbose-hello-world-api/src/test/java/com/github/jinahya/hello/api/_java_util_zip/HelloWorld_Java_Util_Zip__Test.java
+++ 01-verbose-hello-world-api/src/test/java/com/github/jinahya/hello/api/_java_util_zip/HelloWorld_Java_Util_Zip__Test.java
@@ -10,11 +10,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Base64;
 import java.util.zip.Deflater;
@@ -22,16 +26,78 @@ import java.util.zip.DeflaterOutputStream;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 import java.util.zip.InflaterInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 @Slf4j
 class HelloWorld_Java_Util_Zip__Test
         extends HelloWorldTest {
 
+    @TempDir
+    private static File tempDir;
+
+    // ---------------------------------------------------------------------------------------------
     @BeforeEach
     void __() throws IOException {
         HelloWorldTestUtils.set_array_sets_actual_hello_world_bytes(service());
         HelloWorldTestUtils.write_stream_will_write_actual_hello_world_bytes(service());
+    }
+
+    @Nested
+    class ZipOutputStream_Test {
+
+        @Test
+        void __ZipInputStream() throws IOException {
+            // ----------------------------------------------------------------------------- given
+            final var tempFile = File.createTempFile("tmp", null, tempDir);
+            // ------------------------------------------------------------------------------ when
+            try (var fos = new FileOutputStream(tempFile);
+                 var zos = new ZipOutputStream(fos)) {
+                zos.putNextEntry(new ZipEntry("hello-world.txt"));
+                service().write(zos);
+                zos.closeEntry();
+                zos.flush();
+            }
+            // ------------------------------------------------------------------------------ then
+            try (var fis = new FileInputStream(tempFile);
+                 var zis = new ZipInputStream(fis)) {
+                final var entry = zis.getNextEntry();
+                assert entry != null;
+                Assertions.assertArrayEquals(
+                        HelloWorldTestUtils.hello_world_byte_array(),
+                        zis.readAllBytes()
+                );
+            }
+        }
+
+        @Test
+        void __ZipFile() throws IOException {
+            // ----------------------------------------------------------------------------- given
+            final var tempFile = File.createTempFile("tmp", null, tempDir);
+            final var entryName = "hello-world.bin";
+            // ------------------------------------------------------------------------------ when
+            try (var fos = new FileOutputStream(tempFile);
+                 var zos = new ZipOutputStream(fos)) {
+                zos.putNextEntry(new ZipEntry(entryName));
+                service().write(zos);
+                zos.closeEntry();
+                zos.flush();
+            }
+            // ------------------------------------------------------------------------------ then
+            try (var zipFile = new ZipFile(tempFile)) {
+                final var entry = zipFile.getEntry(entryName);
+                assert entry != null;
+                try (var in = zipFile.getInputStream(entry)) {
+                    Assertions.assertArrayEquals(
+                            HelloWorldTestUtils.hello_world_byte_array(),
+                            in.readAllBytes()
+                    );
+                }
+            }
+        }
     }
 
     // ---------------------------------------------------------------------------------------------
