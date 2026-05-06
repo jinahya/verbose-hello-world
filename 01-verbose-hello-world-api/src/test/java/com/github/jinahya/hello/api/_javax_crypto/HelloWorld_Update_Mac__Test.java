ac9ce649a5bb9310425df595780b077d15b4cc5a
Jin Kwon <onacit@gmail.com>
2026-05-07 00:00:20 +0900

    wip

--- 01-verbose-hello-world-api/src/test/java/com/github/jinahya/hello/api/_javax_crypto/HelloWorld_Update_Mac__Test.java
+++ 01-verbose-hello-world-api/src/test/java/com/github/jinahya/hello/api/_javax_crypto/HelloWorld_Update_Mac__Test.java
@@ -2,8 +2,13 @@ package com.github.jinahya.hello.api._javax_crypto;
 
 import com.github.jinahya.hello.api.HelloWorldTest;
 import com.github.jinahya.hello.api.HelloWorldTestUtils;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 
@@ -12,66 +17,22 @@ import javax.crypto.Mac;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.PBEParameterSpec;
-import java.security.Key;
+import java.util.Base64;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
-/**
- * Tests for {@link com.github.jinahya.hello.api.HelloWorld#update(Mac)} method.
- *
- * @author Jin Kwon &lt;onacit_at_gmail.com&gt;
- * @see <a
- * href="https://docs.oracle.com/en/java/javase/25/docs/api/java.base/javax/crypto/Mac.html">javax.crypto.Mac</a>
- * @see <a
- * href="https://docs.oracle.com/en/java/javase/25/docs/specs/security/standard-names.html#mac-algorithms">Mac
- * Algorithms</a> (Java Security Standard Algorithm Names)
- */
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
 @Slf4j
 class HelloWorld_Update_Mac__Test
         extends HelloWorldTest {
 
-    // https://docs.oracle.com/en/java/javase/25/docs/specs/security/standard-names.html#mac-algorithms
-    // HmacMD5
-    // HmacSHA1
-    // HmacSHA224
-    // HmacSHA256
-    // HmacSHA384
-    // HmacSHA512
-    // HmacSHA512/224
-    // HmacSHA512/256
-    // HmacSHA3-224
-    // HmacSHA3-256
-    // HmacSHA3-384
-    // HmacSHA3-512
-    // ---------------------------------------------------------------------------------------------
-    // PBEWithHmac<digest>
-    // ---------------------------------------------------------------------------------------------
-    // HmacPBESHA1
-    // HmacPBESHA224
-    // HmacPBESHA256
-    // HmacPBESHA384
-    // HmacPBESHA512
-    // HmacPBESHA512/224
-    // HmacPBESHA512/256
-
-    private static Key generateSecretKey(final String algorithm)
-            throws Exception {
-        final var generator = KeyGenerator.getInstance(algorithm);
-        return generator.generateKey();
-    }
-
-    private static Key generatePBESecretKey(final String keyFactoryAlgorithm)
-            throws Exception {
-        final var password = "password".toCharArray();
-        final var keySpec = new PBEKeySpec(password);
-        final var factory = SecretKeyFactory.getInstance(keyFactoryAlgorithm);
-        return factory.generateSecret(keySpec);
-    }
-
-    private static PBEParameterSpec generatePBEParameterSpec() {
-        final var salt = new byte[16];
-        ThreadLocalRandom.current().nextBytes(salt);
-        final var iterationCount = 1000;
-        return new PBEParameterSpec(salt, iterationCount);
+    private static void printf(final String algorithm, final Object parameter, final byte[] tag) {
+        final var encoded = Base64.getEncoder().encodeToString(tag);
+        System.out.printf("%30s %20s (%4d) %s...%s%n", algorithm,
+                          Optional.ofNullable(parameter).orElse(""),
+                          tag.length,
+                          encoded.substring(0, 4),
+                          encoded.substring(encoded.length() - 4));
     }
 
     // ---------------------------------------------------------------------------------------------
@@ -83,117 +44,76 @@ class HelloWorld_Update_Mac__Test
             return mac;
         }).when(service()).update(ArgumentMatchers.<Mac>notNull());
     }
-//
-//    // =============================================================================================
-//    // Hmac<digest>
-//    // =============================================================================================
-//    private static Stream<String> getHmacAlgorithmsStream() {
-//        return Stream.of(
-//                "HmacMD5",
-//                "HmacSHA1",
-//                "HmacSHA224",
-//                "HmacSHA256",
-//                "HmacSHA384",
-//                "HmacSHA512",
-//                "HmacSHA512/224",
-//                "HmacSHA512/256",
-//                "HmacSHA3-224",
-//                "HmacSHA3-256",
-//                "HmacSHA3-384",
-//                "HmacSHA3-512"
-//        );
-//    }
-//
-//    @DisplayName("Hmac<digest>")
-//    @Nested
-//    class Hmac_Test {
-//
-//        @MethodSource(
-//                "com.github.jinahya.hello.api._javax_crypto.HelloWorld_Update_Mac_Test#getHmacAlgorithmsStream")
-//        @ParameterizedTest
-//        void __(final String algorithm)
-//                throws Exception {
-//            // ------------------------------------------------------------------------------- given
-//            final var service = service();
-//            final var key = generateSecretKey(algorithm);
-//            final var mac = Mac.getInstance(algorithm);
-//            mac.init(key);
-//            // -------------------------------------------------------------------------------- when
-//            final var result = service.update(mac);
-//            // -------------------------------------------------------------------------------- then
-//            Assertions.assertSame(mac, result);
-//            final var finalized = mac.doFinal();
-//            log.debug("{}: [{}] ({} bytes, {} bits)", algorithm,
-//                      HexFormat.of().formatHex(finalized), finalized.length >> 3, finalized.length);
-//        }
-//    }
-//
-//    // =============================================================================================
-//    // PBEWithHmac<digest> / HmacPBE<digest>
-//    // =============================================================================================
-//    private static Stream<String> getPbeDigestsStream() {
-//        return Stream.of(
-//                "SHA1",
-//                "SHA224",
-//                "SHA256",
-//                "SHA384",
-//                "SHA512",
-//                "SHA512/224",
-//                "SHA512/256"
-//        );
-//    }
-//
-//    @DisplayName("PBEWithHmac<digest>")
-//    @Nested
-//    class PBEWithHmac_Test {
-//
-//        @MethodSource(
-//                "com.github.jinahya.hello.api._javax_crypto.HelloWorld_Update_Mac_Test#getPbeDigestsStream")
-//        @ParameterizedTest
-//        void __(final String digest)
-//                throws Exception {
-//            // ------------------------------------------------------------------------------- given
-//            final var service = service();
-//            final var algorithm = "PBEWithHmac" + digest;
-//            final var keyFactoryAlgorithm = algorithm + "AndAES_256";
-//            final var key = generatePBESecretKey(keyFactoryAlgorithm);
-//            final var params = generatePBEParameterSpec();
-//            final var mac = Mac.getInstance(algorithm);
-//            mac.init(key, params);
-//            // -------------------------------------------------------------------------------- when
-//            final var result = service.update(mac);
-//            // -------------------------------------------------------------------------------- then
-//            Assertions.assertSame(mac, result);
-//            final var finalized = mac.doFinal();
-//            log.debug("{}: [{}] ({} bytes, {} bits)", algorithm,
-//                      HexFormat.of().formatHex(finalized), finalized.length >> 3, finalized.length);
-//        }
-//    }
-//
-//    @DisplayName("HmacPBE<digest>")
-//    @Nested
-//    class HmacPBE_Test {
-//
-//        @MethodSource(
-//                "com.github.jinahya.hello.api._javax_crypto.HelloWorld_Update_Mac_Test#getPbeDigestsStream")
-//        @ParameterizedTest
-//        void __(final String digest)
-//                throws Exception {
-//            // ------------------------------------------------------------------------------- given
-//            final var service = service();
-//            final var algorithm = "HmacPBE" + digest;
-//            final var keyFactoryAlgorithm = "PBEWithHmac" + digest + "AndAES_256";
-//            final var key = generatePBESecretKey(keyFactoryAlgorithm);
-//            final var params = generatePBEParameterSpec();
-//            final var mac = Mac.getInstance(algorithm);
-//            mac.init(key, params);
-//            // -------------------------------------------------------------------------------- when
-//            final var result = service.update(mac);
-//            // -------------------------------------------------------------------------------- then
-//            Assertions.assertSame(mac, result);
-//            final var finalized = mac.doFinal();
-//            log.debug("{}: [{}] ({} bytes, {} bits)", algorithm,
-//                      HexFormat.of().formatHex(finalized), finalized.length >> 3, finalized.length);
-//        }
-//    }
+
+    // ---------------------------------------------------------------------------------------------
+    @DisplayName("HmacSHA1")
+    @Nested
+    class HmacSha1_Test {
+
+        private static final String ALGORITHM = "HmacSHA1";
+
+        @Test
+        void __() throws Exception {
+            // ------------------------------------------------------------------------------- given
+            final var generator = KeyGenerator.getInstance(ALGORITHM);
+            final var key = generator.generateKey();
+            final var mac = Mac.getInstance(ALGORITHM);
+            mac.init(key);
+            // -------------------------------------------------------------------------------- when
+            service().update(mac);
+            final var tag = mac.doFinal();
+            // -------------------------------------------------------------------------------- then
+            printf(ALGORITHM, null, tag);
+        }
+    }
+
+    @DisplayName("HmacSHA256")
+    @Nested
+    class HmacSHA256_Test {
+
+        private static final String ALGORITHM = "HmacSHA256";
+
+        @Test
+        void __() throws Exception {
+            // ------------------------------------------------------------------------------- given
+            final var generator = KeyGenerator.getInstance(ALGORITHM);
+            final var key = generator.generateKey();
+            final var mac = Mac.getInstance(ALGORITHM);
+            mac.init(key);
+            // -------------------------------------------------------------------------------- when
+            service().update(mac);
+            final var tag = mac.doFinal();
+            // -------------------------------------------------------------------------------- then
+            printf(ALGORITHM, null, tag);
+        }
+    }
+
+    @DisplayName("PBEWithHmacSHA256")
+    @Nested
+    class PBEWithHmacSHA256_Test {
+
+        private static final String ALGORITHM = "PBEWithHmacSHA256";
+
+        private static final String KEY_FACTORY_ALGORITHM = ALGORITHM + "AndAES_256";
+
+        @Test
+        void __() throws Exception {
+            // ------------------------------------------------------------------------------- given
+            final var password = "password".toCharArray();
+            final var keySpec = new PBEKeySpec(password);
+            final var factory = SecretKeyFactory.getInstance(KEY_FACTORY_ALGORITHM);
+            final var key = factory.generateSecret(keySpec);
+            final var salt = new byte[16];
+            ThreadLocalRandom.current().nextBytes(salt);
+            final var iterationCount = 1000;
+            final var params = new PBEParameterSpec(salt, iterationCount);
+            final var mac = Mac.getInstance(ALGORITHM);
+            mac.init(key, params);
+            // -------------------------------------------------------------------------------- when
+            service().update(mac);
+            final var tag = mac.doFinal();
+            // -------------------------------------------------------------------------------- then
+            printf(ALGORITHM, iterationCount, tag);
+        }
+    }
 }
