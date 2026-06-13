04500bc91ee9710231f76d7a30680559613c911f
Jin Kwon <onacit@gmail.com>
2026-06-13 20:45:14 +0900

    wip

--- 01-verbose-hello-world-api/src/main/java/com/github/jinahya/hello/api/HelloWorldBytePublisher.java
+++ 01-verbose-hello-world-api/src/main/java/com/github/jinahya/hello/api/HelloWorldBytePublisher.java
@@ -20,7 +20,6 @@ package com.github.jinahya.hello.api;
  * #L%
  */
 
-import java.lang.invoke.*;
 import java.util.*;
 import java.util.concurrent.*;
 
@@ -90,10 +89,6 @@ import java.util.concurrent.*;
  * @see HelloWorldArrayPublisher
  */
 public class HelloWorldBytePublisher implements Flow.Publisher<Byte> {
-
-    private static final System.Logger logger = System.getLogger(
-            MethodHandles.lookup().lookupClass().getName()
-    );
 
     /**
      * Creates a new instance backed by the specified service, dispatching the producer task and the
